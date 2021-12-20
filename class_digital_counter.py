25acaf8224610744c8b0e16891b9a009103a1f2c
Sergey Komanich <komanichs@gmail.com>
2021-12-20 10:57:36 +0200

    ClassCounter

---
+++ class_digital_counter.py
@@ -0,0 +1,28 @@
+"""
+Реализовать класс цифрового счетчика. В классе реализовать методы:
+установки максимального, минимального и начального значения счётчика
+увеличения счетчика на 1
+возвращения текущего значения счётчика
+"""
+
+class Counter:
+
+    def __init__(self, start_value, stop_value):
+        self.current_position = start_value
+        self.stop_counter = stop_value
+        self.current_position = 0
+
+    def increase_counter(self):
+        if self.current_position < self.stop_counter:
+            self.current_position += 1
+        return self.current_position
+
+    def coun_position_now(self) -> int:
+        return self.current_position
+
+
+counter = Counter(0, 10)
+print(counter.increase_counter())
+print(counter.increase_counter())
+print(counter.increase_counter())
+print('Current counter state now:', counter.coun_position_now())
